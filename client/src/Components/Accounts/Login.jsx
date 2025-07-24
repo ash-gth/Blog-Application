fa7f8db199c87989963e70449cfbfb645e809e34
ash-gth <ashisharma9318@gmail.com>
2025-07-24 23:50:46 +0530

    MUI Installation and Login Page

--- client/src/Components/Accounts/Login.jsx
+++ client/src/Components/Accounts/Login.jsx
@@ -1,10 +1,11 @@
 
-
+import {Box, box} from '@mui/material'
 
 const Login = () =>
 {
+    const imageURL = 'https://www.sesta.it/wp-content/uploads/2021/03/logo-blog-sesta-trasparente.png'
     return(
-        <p>Hello from Login </p>
+      <Box><img src={imageURL} alt="login" /></Box>
     )
 }
 
