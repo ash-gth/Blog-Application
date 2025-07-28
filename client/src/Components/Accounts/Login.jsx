d59d1959d3614de5ef5d7ea4ac7273055f597264
ash-gth <ashisharma9318@gmail.com>
2025-07-28 12:21:59 +0530

    login page HTML & CSS complete

--- client/src/Components/Accounts/Login.jsx
+++ client/src/Components/Accounts/Login.jsx
@@ -1,12 +1,111 @@
+import React, { useState, useEffect, useContext } from 'react';
+import { TextField, Box, Button, Typography, styled } from '@mui/material';
 
-import {Box, box} from '@mui/material'
 
-const Login = () =>
-{
-    const imageURL = 'https://www.sesta.it/wp-content/uploads/2021/03/logo-blog-sesta-trasparente.png'
-    return(
-      <Box><img src={imageURL} alt="login" /></Box>
+const Component = styled(Box)`
+    width: 400px;
+    margin: auto;
+    box-shadow: 5px 2px 5px 2px rgb(0 0 0/ 0.6);
+`;
+
+const Image = styled('img')({
+    width: 100,
+    display: 'flex',
+    margin: 'auto',
+    padding: '50px 0 0'
+});
+
+const Wrapper = styled(Box)`
+    padding: 25px 35px;
+    display: flex;
+    flex: 1;
+    overflow: auto;
+    flex-direction: column;
+    & > div, & > button, & > p {
+        margin-top: 20px;
+    }
+`;
+
+const LoginButton = styled(Button)`
+    text-transform: none;
+    background: #FB641B;
+    color: #fff;
+    height: 48px;
+    border-radius: 2px;
+`;
+
+const SignupButton = styled(Button)`
+    text-transform: none;
+    background: #fff;
+    color: #2874f0;
+    height: 48px;
+    border-radius: 2px;
+    box-shadow: 0 2px 4px 0 rgb(0 0 0 / 20%);
+`;
+
+const Text = styled(Typography)`
+    color: #878787;
+    font-size: 12px;
+`;
+
+const Error = styled(Typography)`
+    font-size: 10px;
+    color: #ff6161;
+    line-height: 0;
+    margin-top: 10px;
+    font-weight: 600;
+`
+
+const loginInitialValues = {
+    username: '',
+    password: ''
+};
+
+const signupInitialValues = {
+    name: '',
+    username: '',
+    password: '',
+};
+
+const Login = ({ isUserAuthenticated }) => {
+    const [login, setLogin] = useState(loginInitialValues);
+    const [signup, setSignup] = useState(signupInitialValues);
+    const [error, showError] = useState('');
+    const [account, toggleAccount] = useState('login');
+    const imageURL = 'https://www.sesta.it/wp-content/uploads/2021/03/logo-blog-sesta-trasparente.png';
+ return(
+
+  <Component>
+            <Box>
+                <Image src={imageURL} alt="blog" />
+                {
+                    account === 'login' ?
+                        <Wrapper>
+                            <TextField variant="standard" value={login.username}  name='username' label='Enter Username' />
+                            <TextField variant="standard" value={login.password}  name='password' label='Enter Password' />
+
+                            {error && <Error>{error}</Error>}
+
+                            <LoginButton variant="contained"  >Login</LoginButton>
+                            <Text style={{ textAlign: 'center' }}>OR</Text>
+                            <SignupButton  style={{ marginBottom: 50 }}>Create an account</SignupButton>
+                        </Wrapper> :
+                        <Wrapper>
+                            <TextField variant="standard"  name='name' label='Enter Name' />
+                            <TextField variant="standard"  name='username' label='Enter Username' />
+                            <TextField variant="standard"  name='password' label='Enter Password' />
+
+                            <SignupButton >Signup</SignupButton>
+                            <Text style={{ textAlign: 'center' }}>OR</Text>
+                            <LoginButton variant="contained" >Already have an account</LoginButton>
+                        </Wrapper>
+                }
+            </Box>
+        </Component>
+       
     )
-}
+ 
+  }
 
-export default Login
+
+export default Login;
